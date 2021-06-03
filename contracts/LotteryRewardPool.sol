53f1e1b524314fbf5632b86ca4d3393b381ee9e3
mobile-dev-hybrid <cutecate2019118@gmail.com>
2021-06-03 18:51:00 +0300

    updated farm for soy

--- contracts/LotteryRewardPool.sol
+++ contracts/LotteryRewardPool.sol
@@ -20,16 +20,16 @@ contract LotteryRewardPool is Ownable {
     address public adminAddress;
     address public receiver;
     IBEP20 public lptoken;
-    IBEP20 public cake;
+    IBEP20 public soy;
 
     constructor(
         MasterChef _chef,
-        IBEP20 _cake,
+        IBEP20 _soy,
         address _admin,
         address _receiver
     ) public {
         chef = _chef;
-        cake = _cake;
+        soy = _soy;
         adminAddress = _admin;
         receiver = _receiver;
     }
@@ -51,8 +51,8 @@ contract LotteryRewardPool is Ownable {
 
     function  harvest(uint256 _pid) external onlyAdmin {
         chef.deposit(_pid, 0);
-        uint256 balance = cake.balanceOf(address(this));
-        cake.safeTransfer(receiver, balance);
+        uint256 balance = soy.balanceOf(address(this));
+        soy.safeTransfer(receiver, balance);
         emit Harvest(msg.sender, _pid);
     }
 
@@ -61,12 +61,12 @@ contract LotteryRewardPool is Ownable {
     }
 
     function  pendingReward(uint256 _pid) external view returns (uint256) {
-        return chef.pendingCake(_pid, address(this));
+        return chef.pendingSoy(_pid, address(this));
     }
 
     // EMERGENCY ONLY.
     function emergencyWithdraw(IBEP20 _token, uint256 _amount) external onlyOwner {
-        cake.safeTransfer(address(msg.sender), _amount);
+        soy.safeTransfer(address(msg.sender), _amount);
         emit EmergencyWithdraw(msg.sender, _amount);
     }
 
